5bcd705daa93a0c0f1ef6e41dd92cf26d9c6aa54
Dave Schilling <ds@daves-mbp.wusd.local>
2018-10-18 12:22:39 -0700

    added spoilers and img

---
+++ vape.js
@@ -0,0 +1,17 @@
+// When the button is pressed
+$(".spoiler").on("click", "button", function() {
+  //Show the spoiler text
+  $(this)
+    .prev()
+    .show();
+  //Hide the "Reveal Spoiler" button
+  $(this).hide();
+});
+
+// Create the "Reveal Spoiler" button
+const $button = $("<button>Fun Fact</button>");
+//Append to web page
+$(".spoiler").append($button);
+
+//Hide the spoiler text
+$(".spoiler span").hide();
